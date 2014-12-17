56b9f20a51bfe390d4550ede52a31f949c2053ab
yourmoonlight <foxkingmoonlight@gmail.com>
2014-12-17 16:00:46 +0800

    valid parentheses

---
+++ valid_parentheses.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isValid(self, s):
+        stack = []
+        for i in range(len(s)):
+            if s[i] == "(" or s[i] == "[" or s[i] == "{":
+                stack.append(s[i])
+            if s[i] == ")":
+                if stack == [] or stack.pop != "(":
+                    return False
+            if s[i] == "]":
+                if stack == [] or stack.pop != "[":
+                    return False
+            if s[i] == "}":
+                if stack == [] or stack.pop != "{":
+                    return False
+        if stack != []:
+            return False
+        else:
+            return True
